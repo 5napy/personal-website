47740360e776fdac68970a606bc2111df1bb6cd1
Gustavo <gusbz.117@gmail.com>
2026-01-17 14:17:04 -0600

    added more projects

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -3,25 +3,38 @@ import quoteImg from "./quoteMachine.webp";
 import calculatorImg from "./calculator.webp";
 import markdownImg from "./markdownPre.webp";
 import drumMachine from "./drumMachine.webp";
+import palindromeChecker from "./palindrome.webp";
+import rpgCreature from "./rpg_creature.webp";
+import romanNumeral from "./roman_numeral.webp";
+import musicPlayer from "./music_player.webp";
+
+
 
 
 const projects = [
   {
     id: 1,
     title: "Pomodoro Timer",
-    description: "Timer app in React for the Pomodoro Technique",
+    description: "Timer app built in React for the Pomodoro Technique",
     link: "https://5napy.github.io/pomodoro-clock",
     image: pomodoroImg,
   },
-  {
+      {
     id: 2,
+    title: "RPG Creature Search",
+    description: "RPG creature finder that fetches data from an API",
+    link: "https://5napy.github.io/rpg-creature-finder/",
+    image: rpgCreature,
+  },
+  {
+    id: 3,
     title: "Quote Generator",
     description: "Random quote machine built in React",
     link: "https://5napy.github.io/Random-quote-machine",
     image: quoteImg,
   },
   {
-    id: 3,
+    id: 4,
     title: "Calculator",
     description: "Basic Calculator built in React",
     link: "https://5napy.github.io/calculator-fcc",
@@ -29,19 +42,40 @@ const projects = [
 
   },
   {
-    id: 4,
+    id: 5,
     title: "Markdown Previewer",
     description: "Makrdown previewer built in React",
     link: "https://5napy.github.io/markdown-previewer-fcc",
     image: markdownImg,
   },
   {
-    id: 5,
+    id: 6,
     title: "Drum Machine",
     description: "Drum Machine Project built in React",
     link: "https://5napy.github.io/drum-machine-fcc/",
     image: drumMachine,
   },
-];
+  {
+    id: 7,
+    title: "Palindrome Checker",
+    description: "Palindrome checker app build with JavaScript",
+    link: "https://5napy.github.io/palindrome-checker/",
+    image: palindromeChecker,
+  },  
+  {
+    id: 8,
+    title: "Roman Numeral Converter",
+    description: "A JavaScript app that converts numbers into their Roman numeral representation using standard Roman numeral rules.",
+    link: "https://5napy.github.io/roman-numeral-converter/",
+    image: romanNumeral,
+  },
+    {
+    id: 9,
+    title: "Simple Music Player",
+    description: "Music player app with pre-loaded data built in JavaScript",
+    link: "https://5napy.github.io/simple-music-player/",
+    image: musicPlayer,
+  },
 
+];
 export default projects;
